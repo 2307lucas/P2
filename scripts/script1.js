883cd0831cac1bf228a5c1ff01ae33c5fc61471b
2307lucas <92086095+2307lucas@users.noreply.github.com>
2022-03-26 23:11:13 +0100

    New

---
+++ scripts/script1.js
@@ -0,0 +1,39 @@
+
+var objPeople = [
+    {
+        username: "Simon",
+        password: "1234"
+    },
+    {
+        username: "Nikolaj",
+        password: "5678"
+    },
+    {
+        username: "Søren",
+        password: "1567"
+    }
+]
+
+
+
+
+function logIn() {
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+
+    for (i = 0; i <objPeople.length; i++){
+        if (username === objPeople[i].username && password === objPeople[i].password) {
+            console.log(username + "\tis logged in")
+        } else {
+            console.log("Incorrect username or password")
+        }
+    }
+
+
+
+
+
+
+
+
+}
